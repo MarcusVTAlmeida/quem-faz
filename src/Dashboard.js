e4f82aba40cdad1d4d96f266d93072f59b545e61
unknown <marcus1134@hotmail.com>
2022-05-10 03:33:33 -0300

    meu terceiro commit

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,36 +1,68 @@
-import "./App.css";
-import React, { useState } from "react";
-import JsonData from "./MOCK_DATA.json";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Dashboard.css";
+import { auth, db, logout } from "./firebase";
+import { query, collection, getDocs, where, doc, addDoc, setDoc, updateDoc, getDoc, onSnapshot } from "firebase/firestore";
+import debounce from "lodash.debounce";
 import ReactPaginate from "react-paginate";
 
-function App() {
-  const [users, setUsers] = useState(JsonData.slice(0, 50));
+const getFilteredItems = (query, tasks) => {
+  if (!query) {
+    return tasks;
+  }
+  return tasks.filter((user) => user.cidade.toLowerCase().includes(query.toLowerCase()));
+};
+
+function Dashboard() {
+  const [tasks, setTasks] = useState([])
+  const [query, setQuery] = useState("")
+  const navigate = useNavigate();
   const [pageNumber, setPageNumber] = useState(0);
 
-  const usersPerPage = 10;
+  const usersPerPage = 2;
   const pagesVisited = pageNumber * usersPerPage;
 
-  const displayUsers = users
-    .slice(pagesVisited, pagesVisited + usersPerPage)
-    .map((user) => {
-      return (
-        <div className="user">
-          <h3>{user.firstName}</h3>
-          <h3>{user.lastName}</h3>
-          <h3>{user.email}</h3>
-        </div>
-      );
-    });
-
-  const pageCount = Math.ceil(users.length / usersPerPage);
+   const pageCount = Math.ceil(tasks.length / usersPerPage);
 
   const changePage = ({ selected }) => {
     setPageNumber(selected);
   };
 
+  const usersCollectionRef = collection(db, "users");
+  const filteredItems = getFilteredItems(query, tasks);
+  const updateQuery = (e) => setQuery(e?.target?.value);
+  const debouncedOnChange = debounce(updateQuery, 200);
+
+  const getUsers = async () => {
+    const data = await getDocs(usersCollectionRef);
+    setTasks(data.docs.map((doc) => ({ ...doc.data(), id: doc.uid })));
+  };
+
+  useEffect(() => {
+    getUsers()
+    
+  }, []);
+
   return (
     <div className="App">
-      {displayUsers}
+      <div className="dashboard__container">
+        <div>
+          <Link to="/login">Login</Link>
+        </div>
+        <div>
+          <Link to="/resgister">Registrar</Link>
+        </div>
+        Encontre um prestador de serviço
+        <input type="search" onChange={debouncedOnChange} autoCapitalize='characters' />
+        {filteredItems.slice(pagesVisited, pagesVisited + usersPerPage).map((user) => (
+          <div>
+            <div style={{ border: "5px solid", borderColor: "#a36a00" }} />
+            <h3>Nome: {user.name}</h3>
+            <h3>Cidade: {user.cidade}</h3>
+            <h3>Email: {user.email}</h3>
+          </div>
+        ))}
+     
       <ReactPaginate
         previousLabel={"Previous"}
         nextLabel={"Next"}
@@ -42,8 +74,9 @@ function App() {
         disabledClassName={"paginationDisabled"}
         activeClassName={"paginationActive"}
       />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Dashboard;
